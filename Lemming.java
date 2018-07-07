92a0f91259fd6d7cf885bb505d773fdfd919604f
vivekTestGit <vivek_4viv@yahoo.co.in>
2018-07-06 20:40:32 -0500

    Testing how to commit through terminal

---
+++ Lemming.java
@@ -0,0 +1,51 @@
+public class Lemming {
+	
+	
+	public static void main(String[] args) {
+		
+		int dayoftheweek=2;
+		
+		String schedule;
+		
+		switch (dayoftheweek) {
+		case 1:
+		schedule ="Gym in the morning.";
+		break;
+		case 2: 
+		schedule="Class after work.";
+		break;
+		case 3: 
+	    schedule="Meetings all day.";
+		break;
+		case 4:
+		schedule="Work from home.";
+		break;
+		case 5:
+		schedule="Game night after work.";
+		break;
+		case 6: case 7: default:
+		schedule = "Free!";
+		break;
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		System.out.println(schedule);
+		
+
+		
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+}
